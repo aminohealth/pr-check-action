55a4092ac2af7505f3092b3e77827928c72de1ad
Elliott <elliott@amino.com>
2021-03-05 15:40:25 -0800

    Update index to use new inputs

--- index.js
+++ index.js
@@ -1,60 +1,73 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 async function run() {
-    try {
-        const linkRegExInput = core.getInput('link-regex');
-        const linkRegExp = new RegExp(linkRegExInput)
+  try {
+    const linkRegExInput = core.getInput("link-regex");
+    const linkRegExp = new RegExp(linkRegExInput);
 
-        //matching in pr comments OR body of pr
-        let matchedStrings = [];
+    // These inputs are always strings
+    const isDebug = core.getInput("debug").toLowerCase() === "true";
 
-        console.log(`Checking for links: ${linkRegExInput}!`);
+    //matching in pr comments OR body of pr
+    let matchedStrings = [];
 
-        const token = core.getInput('repo-token');
-        const octokit = new github.GitHub(token);
-        const payload = github.context.payload;
+    core.info(`Checking for links matching: ${linkRegExInput}!`);
 
-        // personal repos have no org - allow for those as well as team ones
-        const owner = (payload.organization || payload.repository.owner).login;
-        const repo = payload.repository.name;
+    const token = core.getInput("repo-token");
+    const octokit = new github.GitHub(token);
+    const payload = github.context.payload;
 
-        // issue events (like create comment) supply #issue instead of #pull_request - handle both
-        const issue_number = (payload.pull_request || payload.issue).number;
-        
-        const issuesArgs = { owner, repo, issue_number };
+    // personal repos have no org - allow for those as well as team ones
+    const owner = (payload.organization || payload.repository.owner).login;
+    const repo = payload.repository.name;
 
-        const pull = await octokit.issues.get(issuesArgs);
-        console.log(`pr body: ${pull.data.body}`);
+    // issue events (like create comment) supply #issue instead of #pull_request - handle both
+    const issue_number = (payload.pull_request || payload.issue).number;
 
-        const bodyMatches = linkRegExp.exec(pull.data.body);
-        if(bodyMatches) matchedStrings.push(bodyMatches[0]);
-        
-        if(matchedStrings.length < 1) {
-            const prComments = await octokit.issues.listComments(issuesArgs);
+    const issuesArgs = { owner, repo, issue_number };
 
-            const commentsWithLinks = prComments.data.reduce((acc, curr) => {
-                const matches = linkRegExp.exec(curr.body);
-                if(matches) acc.push(matches[0]);
-                return acc;
-            }, []);
-            
-            matchedStrings = matchedStrings.concat(commentsWithLinks);
-          
-            console.log('all prComments:')
-            console.log(prComments.data.map(i => i.body));
-        }
+    const pull = await octokit.issues.get(issuesArgs);
+    isDebug && console.log(`PR Body: \n${pull.data.body}\n`);
 
-        console.log('matches:')
-        console.log(matchedStrings);
-
-        core.setOutput('msg', `Found ${matchedStrings.length} with matching links`)
-
-        if(matchedStrings.length === 0) core.setFailed(`unable to find any comments matching link ${linkRegExInput}`)
-    } catch (error) {
-          console.log(error);
-          core.setFailed(error.message);
+    const bodyMatches = linkRegExp.exec(pull.data.body);
+    if (bodyMatches) {
+      matchedStrings.push(bodyMatches[0]);
     }
+
+    const includeComments =
+      core.getInput("include-comments").toLowerCase() === "true";
+    if (includeComments && matchedStrings.length < 1) {
+      const prComments = await octokit.issues.listComments(issuesArgs);
+
+      const commentsWithLinks = prComments.data.reduce((acc, curr) => {
+        const matches = linkRegExp.exec(curr.body);
+        if (matches) acc.push(matches[0]);
+        return acc;
+      }, []);
+
+      matchedStrings = matchedStrings.concat(commentsWithLinks);
+
+      isDebug && console.log("PR Comments:\n");
+      isDebug && console.log(prComments.data.map((i) => i.body));
+    }
+
+    isDebug && console.log("matches:");
+    isDebug && console.log(matchedStrings);
+
+    const successMessage = core.getInput("success-message");
+    core.setOutput("msg", successMessage);
+
+    if (matchedStrings.length === 0) {
+      const failureMessage = core
+        .getInput("failure-message")
+        .replace("{link-regex}", linkRegExInput);
+      core.setFailed(failureMessage);
+    }
+  } catch (error) {
+    console.log(error);
+    core.setFailed(error.message);
+  }
 }
 
 run();
